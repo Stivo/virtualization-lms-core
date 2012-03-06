cedd35abae8d8bf42f5bf2b3c09396e89547761c
Stefan Ackermann <stivo@Stivo-lap.(none)>
2012-03-06 12:24:10 +0100

    Porting subset of my sources to optiql datastruct branch. No transformers, no hadoop code gen.

---
+++ test-src/epfl/distributed/SparkCodeGen.scala
@@ -0,0 +1,188 @@
+package scala.virtualization.lms
+package epfl
+package distributed
+
+import scala.virtualization.lms.common.ScalaGenBase
+import java.io.PrintWriter
+import scala.reflect.SourceContext
+
+trait SparkProgram extends VectorOpsExp with VectorImplOps with SparkVectorOpsExp {
+  
+}
+
+trait SparkVectorOpsExp extends VectorOpsExp {
+   case class VectorReduceByKey[K: Manifest, V : Manifest](in : Exp[Vector[(K,V)]], func : (Exp[V], Exp[V]) => Exp[V]) 
+    	extends Def[Vector[(K, V)]]{ 
+    //with TypedNode [(K, Iterable[V]),(K,V)]{
+      val mKey = manifest[K]
+      val mValue = manifest[V]
+      lazy val closure = doLambda2(func)(getClosureTypes._2, getClosureTypes._2, getClosureTypes._2)
+      def getClosureTypes = (manifest[(V,V)], manifest[V])
+      
+//      def getTypes = (mKey, mValue)
+    }
+
+    override def syms(e: Any): List[Sym[Any]] = e match { 
+    case s: ClosureNode[_,_] => syms(s.in, s.closure)// ++ super.syms(e) // super call: add case class syms (iff flag is set)
+//    case s: ClosureNode[_,_] => syms(s.in)// ++ super.syms(e) // super call: add case class syms (iff flag is set)
+    case red : VectorReduceByKey[_,_] => syms(red.in, red.closure)
+    case s: VectorReduce[_,_]  => syms(s.in, s.closure) ++ super.syms(e) // super call: add case class syms (iff flag is set)
+    case _ => super.syms(e)
+  }
+
+//   override def readSyms(e: Any): List[Sym[Any]] = e match { //TR FIXME: check this is actually correct
+//    case s: ClosureNode[_,_]  => syms(s.closure, s.in) ++ super.syms(e)// super call: add case class syms (iff flag is set)
+//    case _ => super.readSyms(e)
+//  }
+//  
+//  override def boundSyms(e: Any): List[Sym[Any]] = e match {
+//    case s: ClosureNode[_,_]  => effectSyms(s.closure, s.in) 
+//    case _ => super.boundSyms(e)
+//  }
+
+  
+  override def symsFreq(e: Any): List[(Sym[Any], Double)] = e match {
+    case s: ClosureNode[_,_] => freqNormal(s.in)++freqHot(s.closure) 
+    case red : VectorReduceByKey[_,_] => freqHot(red.closure) ++ freqNormal(red.in)
+//    case s: ClosureNode[_,_] => freqNormal(s.in) 
+//    case s: ClosureNode[_,_]  => freqHot(s.closure) ++ freqNormal(s.in) 
+    case s: VectorReduce[_,_]  => freqHot(s.closure) ++ freqNormal(s.in) 
+    case _ => super.symsFreq(e)
+  }
+  
+      override def mirror[A:Manifest](e: Def[A], f: Transformer): Exp[A] = {
+        (e match {
+        	case v@VectorReduceByKey(vector, func) => toAtom(VectorReduceByKey(f(vector), f(func))(v.mKey, v.mValue))
+        	case _ => super.mirror(e, f)
+        }).asInstanceOf[Exp[A]]
+      }
+}
+
+//trait SparkTransformations extends VectorTransformations {
+//    val IR: VectorOpsExp with SparkVectorOpsExp
+//  	import IR.{VectorReduceByKey, VectorReduce, VectorGroupByKey, VectorMap}
+//    import IR.{Def, Exp}
+//
+//  	class ReduceByKeyTransformation extends SimpleSingleConsumerTransformation {
+//  	  
+//	   def doTransformationPure(inExp : Exp[_]) = inExp match {
+//            case Def(red@VectorReduce(Def(gbk@VectorGroupByKey(v1)),f1)) => {
+//              new VectorReduceByKey(v1, f1)(red.mKey, red.mValue)
+//            }
+//            case _ => null
+//	   }
+//	}
+//
+//  	class PullSparkDependenciesTransformation extends PullDependenciesTransformation {
+//
+// 	   override def appliesToNodeImpl(inExp : Exp[_], t : Transformer) = {
+//		   inExp match {
+//		     case Def(VectorReduceByKey(in, func)) => true
+//		     case _ => super.appliesToNodeImpl(inExp, t)
+//		   }
+//	   }
+// 	   override def doTransformation(inExp : Exp[_]) : Def[_] = inExp match {
+//	     case Def(r@VectorReduceByKey(in, func)) => _doneNodes += inExp; r
+// 	     case _ => super.doTransformation(inExp)
+// 	   }
+//	}
+//
+//  	
+//}
+
+trait SparkGenVector extends ScalaGenBase with ScalaGenVector {
+  
+	val IR: SparkVectorOpsExp
+	import IR.{Sym, Def, Exp, Reify, Reflect, Const}
+	import IR.{NewVector, VectorSave, VectorMap, VectorFilter, VectorFlatMap, VectorFlatten, VectorGroupByKey, VectorReduce
+	  , ComputationNode}
+	import IR.{TTP, TP, SubstTransformer}
+	import IR.{findDefinition}
+	import IR.{ClosureNode, freqHot, freqNormal, Lambda}
+
+	import IR.{GetArgs}
+	import IR.{VectorReduceByKey}
+	import IR.{findDefinition, fresh, reifyEffects, reifyEffectsHere,toAtom}
+		
+  override def emitNode(sym: Sym[Any], rhs: Def[Any])(implicit stream: PrintWriter) = 
+    {val out = rhs match {
+      case nv@NewVector(filename) => emitValDef(sym, "sc.textFile(%s)".format(quote(filename)))
+      case vs@VectorSave(vector, filename) => stream.println("%s.saveAsTextFile(%s)".format(quote(vector), quote(filename)))
+      case vm@VectorMap(vector, function) => emitValDef(sym, "%s.map(%s)".format(quote(vector), quote(vm.closure)))
+      case vf@VectorFilter(vector, function) => emitValDef(sym, "%s.filter(%s)".format(quote(vector), quote(vf.closure)))
+      case vm@VectorFlatMap(vector, function) => emitValDef(sym, "%s.flatMap(%s)".format(quote(vector), quote(vm.closure)))
+      case vm@VectorFlatten(v1) => {
+        var out = "("+v1.map(quote(_)).mkString(").union(")
+        out += ")"
+        emitValDef(sym, out)
+      }
+      case gbk@VectorGroupByKey(vector) => emitValDef(sym, "%s.groupByKey".format(quote(vector)))
+      case red@VectorReduce(vector, f) => emitValDef(sym, "%s.map(x => (x._1,x._2.reduce(%s)))".format(quote(vector), quote(red.closure)))
+      case red@VectorReduceByKey(vector, f) => emitValDef(sym, "%s.reduceByKey(%s)".format(quote(vector), quote(red.closure)))
+      case GetArgs() => emitValDef(sym, "sparkInputArgs.drop(1); // First argument is for spark context")
+    case _ => super.emitNode(sym, rhs)
+  }
+//    println(sym+" "+rhs)
+    out
+    }
+  
+    override def focusExactScopeFat[A](currentScope0: List[TTP])(result0: List[Block[Any]])(body: List[TTP] => A): A = {
+//    val state = new TransformationState(currentScope0, result0)
+//    val transformer = new Transformer(state, List(new MapMergeTransformation()))
+////    buildGraph(transformer)
+//    transformer.stepUntilStable(50)
+//    transformer.transformations = List(new PullDependenciesTransformation())
+//    transformer.stepUntilStable(50)
+//    buildGraph(transformer)
+//    transformer.doOneStep
+    //buildGraph(transformer)
+    super.focusExactScopeFat(currentScope0)(result0)(body)
+  }
+  
+  override def emitSource[A,B](f: Exp[A] => Exp[B], className: String, stream: PrintWriter)(implicit mA: Manifest[A], mB: Manifest[B]): List[(Sym[Any], Any)] = {
+//    val func : Exp[A] => Exp[B] = {x => reifyEffects(f(x))}
+    
+    val x = fresh[A]
+    val y = reifyBlock(f(x))
+
+    val sA = mA.toString
+    val sB = mB.toString
+
+//    val staticData = getFreeDataBlock(y)
+
+    stream.println("/*****************************************\n"+
+                   "  Emitting Spark Code                  \n"+
+                   "*******************************************/")
+    stream.println("""
+package spark.examples;
+import scala.math.random
+import spark._
+import SparkContext._
+
+object %s {
+        def main(sparkInputArgs: Array[String]) {
+    		val sc = new SparkContext(sparkInputArgs(0), "%s")
+        """.format(className, className))
+    	
+    // TODO: separate concerns, should not hard code "pxX" name scheme for static data here
+    
+    emitBlock(y)(stream)
+    
+    
+    stream.println("}")
+    
+    stream.println("}")
+    stream.println("/*****************************************\n"+
+                   "  End of Spark Code                  \n"+
+                   "*******************************************/")
+
+    stream.flush
+    
+//    staticData
+    Nil
+  }
+
+    
+}
+
+trait SparkGen extends VectorBaseCodeGenPkg with SparkGenVector
---
+++ test-src/epfl/distributed/TestPrograms.scala
@@ -0,0 +1,172 @@
+package scala.virtualization.lms
+package epfl
+package distributed
+
+import common._
+import test1._
+import util.OverloadHack
+import java.io.PrintWriter
+import java.io.FileOutputStream
+import java.io.PrintWriter
+import scala.collection.mutable.HashMap
+import internal.GraphVizExport
+import java.io.File
+import java.io.FileWriter
+import java.io.StringWriter
+
+trait VectorsProg extends VectorImplOps {
+  
+  // why need of unit in this example?
+  // How to implement map in terms of flatMap?
+  // use ParallelDo instead of flatMap? Has notion of emitter
+//  
+  def parse( s: Rep[String]) = Integer.parseInt(s)
+//  
+//  def isNumber(s : Rep[String]) = s.matches("\\d+")
+//  
+  def simple(x : Rep[Unit]) = {
+    val words1 = Vector("words1")
+//    words1.filter(_.matches("\\d+"))
+    val parsed = words1.map(parse)
+//    .filter(_>4)
+//    parsed.save("parsed")
+    parsed.map{x => 2*x}
+//    .map(_+"")
+    .save("doubled")
+//    parsed.map{x => x+1}.save("incremented")
+//    .map(_+"asdf")
+//    .save("words")
+    unit("348")
+    //unit(())
+  }
+//  
+//  def wordCount(x: Rep[Unit]) = {
+//    val args = Vector.getArgs
+//	val output = args(1)
+//	val lines = Vector(args(0))
+//    val wordsInLine = lines//.map( _.split("\\s+").apply(1)).flatMap(_.split("[:/_.,]").toSeq)
+////    words.map(_.contains(" ")).save("lines with more than one word")
+//    val wordsTupled = wordsInLine.map((_, unit(1)))
+//    val grouped = wordsTupled.groupByKey//.filter(_._2 > 10)
+//    val counted = grouped.reduce(_+_)
+////    grouped.save(output+"/wordgroups")
+//    counted.save(output+"/wordcounts")
+//
+////    val inverted = counted.map(x => (x._2,x._1))
+//////    val reduced = inverted.reduce((x,y) => if (x._1 > y._1) x else y)
+//////    println(reduced)
+////    val invertedSameKey = inverted.map(x => (unit(0), x))
+////    invertedSameKey.groupByKey.reduce((x,y) => if (x._1 > y._1) x else y)
+////    .map(_._2)
+////    .save(output+"/most common word")
+//    unit(())
+//  }
+//  
+//   def twoStage(x: Rep[Unit]) = {
+//    val words1 = Vector("words1")
+//    val words2 = Vector("words2")
+//    val words3 = Vector("words3")
+//    val wordsInLine = words1++words2++words3//.flatMap( _.split(" ").toSeq)
+////    words.map(_.contains(" ")).save("lines with more than one word")
+//    val wordsTupled = wordsInLine.map((_, unit(1)))
+//    val wordsGrouped = wordsTupled.groupByKey
+//    val counted = wordsGrouped.reduce(_+_)
+//    counted.save("wordcounts")
+//    val inverted = counted.map(x => (x._2,x._1))
+//    inverted.map(x => (unit(0), x))
+//    .groupByKey
+//    .reduce((x,y) => if (x._1 > y._1) x else y)
+//    .map(_._2)
+//    .save("most common word")
+////    val invertedGrouped = inverted.groupByKey
+////    val invertedGrouped2 = Vector[(Int, String)]("Asdf").groupByKey
+//    
+////    val added = invertedGrouped++invertedGrouped2
+////    invertedGrouped
+////    invertedGrouped.save("inverted")
+//  }
+//
+//  
+//  class HadoopContext() {
+//    def getPath() : Rep[String] = unit("asdf")
+//  }
+//  
+//  def flatten( x : Rep[Unit]) = {
+//    val words1 = Vector("words1")
+//    val words2 = Vector("words2")
+//    (((words1++words2).map(_+""))++(Vector("words3"))).save("all words")
+//    unit(())
+//  }
+//  
+//  def logAnalysis(x : Rep[Unit]) = {
+//    val args = Vector.getArgs
+//    val lines = Vector(args(0))
+//    val years = lines.flatMap(_.split(" ").toSeq).flatMap(_.split("/").toSeq)
+//		.filter(_.matches("18\\d{2}"))
+//	years.save(args(1))
+//	unit(())
+//  }
+//  def testFusion(x : Rep[Unit]) = {
+//    val args = Vector.getArgs
+//    val lines = Vector(args(0))
+//    val years = lines.map{y => val x = y.split("\\s+");x(x.length-1)}.map(Integer.parseInt).map(_%7 == 1)
+//	years.save(args(1))
+//	
+//	unit(())
+//  }
+//  def trends(x : Rep[Unit]) = {
+//    val context = new HadoopContext()
+//    val inputPath = "asf"
+//    val lines = Vector(inputPath)
+//    val years = lines.map((context.getPath(),_))
+//	years.save("years")
+//	unit(())
+//  }
+  
+}
+
+
+
+class TestVectors extends FileDiffSuite {
+  
+  def testVectors {
+//    withOutput(System.err/*"test-out/epfl/test-dist"*/) {    
+    try {
+      println("-- begin")
+
+//      val dsl = new VectorsProg with VectorOpsExp 
+////      val codegen = new ScalaGenFunctions with ScalaGenUtil with ScalaGenVector with ScalaGenBase { val IR: dsl.type = dsl }
+////      codegen.emitSource(dsl.test, "g", new PrintWriter(System.out))
+//      val toCompile = dsl.twoStage _
+//      val codegenHadoop = new HadoopGen { val IR: dsl.type = dsl }
+//      codegenHadoop.emitSource(toCompile, "Hadoop", new PrintWriter(System.out))
+//
+//      
+      val writer = new StringWriter()
+      val printer = new PrintWriter(writer)
+      
+      val dslSpark = new VectorsProg with SparkProgram
+      //      val codegenDeps = new HadoopGen { val IR: dsl.type = dsl }
+//      codegenDeps.emitSource(dsl.simple, "g", new PrintWriter(System.out))
+      
+      val codegenSpark = new SparkGen { val IR: dslSpark.type = dslSpark }
+      codegenSpark.emitSource(dslSpark.simple, "Spark", printer)
+      println(writer.toString)
+      
+      val dest = "/home/stivo/master/spark/examples/src/main/scala/spark/examples/SparkGenerated.scala"
+      val fw = new FileWriter(dest)
+      fw.write(writer.toString)
+      fw.close
+      
+      println("-- end")
+//    }
+//      FileOutput.finish
+//      println(FileOutput.sw.toString)
+    } catch {
+      case e => 
+        e.printStackTrace
+        println(e.getMessage)
+    }
+    //assert(true, "did finish")    
+  }
+}
